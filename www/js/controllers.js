be72afc579ec81193ddbb0aaee0a8dec00e817fd
abenyamini <abenyamini@interwise.com>
2014-03-18 18:11:40 +0200

    Fixed version

---
+++ www/js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('starter.controllers', [])
+
+
+// // A simple controller that fetches a list of data from a service
+// .controller('PetIndexCtrl', function($scope, PetService) {
+//   // "Pets" is a service returning mock data (services.js)
+//   $scope.pets = PetService.all();
+// })
+
+
+// // A simple controller that shows a tapped item's data
+// .controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
+//   // "Pets" is a service returning mock data (services.js)
+//   $scope.pet = PetService.get($stateParams.petId);
+// })
+.controller('PollitCtrl', function($scope, $stateParams, PetService) {
+    $scope.header = "pollit"
+})
+
+.controller('VoteitCtrl', function($scope, $stateParams, PetService) {
+    $scope.header = "voteit"
+})
+
+.controller('SettingsCtrl', function($scope, $stateParams, PetService) {
+    $scope.header = "settings"
+})
+
+.controller('DebugCtrl', function($scope, $stateParams, PetService) {
+    $scope.header = "debug"
+});
+
+
+   
+
