997bf3680c96bf21074c94bec4afd2b9cabe33cc
Yael Maoz <ymaoz@interwise.com>
2014-03-19 13:01:24 +0200

    add chart

--- www/js/app.js
+++ www/js/app.js
@@ -5,85 +5,75 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.services', 'starter.controllers', 'chartjs-directive'])
 
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
     // setup an abstract state for the tabs directive
     .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
     })
 
     // the pet tab has its own child nav-view and history
     .state('tab.pollit', {
-      url: '/pollit',
-      views: {
-        'pollit-tab': {
-          templateUrl: 'templates/pollit.html',
-          controller: 'PollitCtrl'
+        url: '/pollit',
+        views: {
+            'pollit-tab': {
+                templateUrl: 'templates/pollit.html',
+                controller: 'PollitCtrl'
+            }
         }
-      }
     })
 
     .state('tab.voteit', {
-      url: '/voteit',
-      views: {
-        'voteit-tab': {
-          templateUrl: 'templates/voteit.html',
-          controller: 'VoteitCtrl'
+        url: '/voteit',
+        views: {
+            'voteit-tab': {
+                templateUrl: 'templates/voteit.html',
+                controller: 'VoteitCtrl'
+            }
         }
-      }
     })
-   .state('tab.settings', {
-      url: '/settings',
-      views: {
-        'settings-tab': {
-          templateUrl: 'templates/settings.html',
-          controller: 'SettingsCtrl'
-        }
-      }
-    })
-   .state('tab.debug', {
-      url: '/debug',
-      views: {
-        'debug-tab': {
-          templateUrl: 'templates/debug.html',
-          controller: 'DebugCtrl'
-        }
-      }
-    })
-    .state('tab.group-polls', {
-      url: '/polls/:groupId',
-      views: {
-        'pollit-tab': {
-          templateUrl: 'templates/group-polls.html',
-          controller: 'GroupPollsCtrl'
-        }
-      }
-    });
+        .state('tab.settings', {
+            url: '/settings',
+            views: {
+                'settings-tab': {
+                    templateUrl: 'templates/settings.html',
+                    controller: 'SettingsCtrl'
+                }
+            }
+        })
+        .state('tab.debug', {
+            url: '/debug',
+            views: {
+                'debug-tab': {
+                    templateUrl: 'templates/debug.html',
+                    controller: 'DebugCtrl'
+                }
+            }
+        })
+        .state('tab.group-polls', {
+            url: '/polls/:groupId',
+            views: {
+                'pollit-tab': {
+                    templateUrl: 'templates/group-polls.html',
+                    controller: 'GroupPollsCtrl'
+                }
+            }
+        });
 
-    // .state('tab.adopt', {
-    //   url: '/adopt',
-    //   views: {
-    //     'adopt-tab': {
-    //       templateUrl: 'templates/adopt.html'
-    //     }
-    //   }
-    // })
 
- 
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/pollit');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/pollit');
 
 });
-
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,13 +27,67 @@ angular.module('starter.controllers', [])
 
 .controller('DebugCtrl', function($scope, $stateParams, PetService) {
     $scope.header = "debug"
+    $scope.MyChart = {
+        width: 80,
+        height: 80,
+
+    }
+
+    $scope.MyChart.options = {
+        //Boolean - Whether we should show a stroke on each segment
+        segmentShowStroke: true,
+
+        //String - The colour of each segment stroke
+        segmentStrokeColor: "#fff",
+
+        //Number - The width of each segment stroke
+        segmentStrokeWidth: 2,
+
+        //The percentage of the chart that we cut out of the middle.
+        percentageInnerCutout: 50,
+
+        //Boolean - Whether we should animate the chart	
+        animation: true,
+
+        //Number - Amount of animation steps
+        animationSteps: 100,
+
+        //String - Animation easing effect
+        animationEasing: "easeOutBounce",
+
+        //Boolean - Whether we animate the rotation of the Doughnut
+        animateRotate: true,
+
+        //Boolean - Whether we animate scaling the Doughnut from the centre
+        animateScale: false,
+
+        //Function - Will fire on animation completion.
+        onAnimationComplete: null
+    }
+
+
+    $scope.MyChart.data = [{
+            value: 30,
+            color: "#F7464A"
+        }, {
+            value: 50,
+            color: "#E2EAE9"
+        }, {
+            value: 100,
+            color: "#D4CCC5"
+        }, {
+            value: 40,
+            color: "#949FB1"
+        }, {
+            value: 120,
+            color: "#4D5360"
+        }
+
+    ]
 })
 
 // A simple controller that shows a tapped item's data
 .controller('GroupPollsCtrl', function($scope, $stateParams, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.id = $stateParams.groupId;
+    // "Pets" is a service returning mock data (services.js)
+    $scope.id = $stateParams.groupId;
 });
-
-   
-
